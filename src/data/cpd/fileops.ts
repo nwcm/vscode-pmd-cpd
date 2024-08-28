3cb822a80a3f0a9ccae0b7580773409720a5fec9
nwcm <111259588+nwcm@users.noreply.github.com>
2024-08-28 17:46:54 +1000

    formatting

--- src/data/cpd/fileops.ts
+++ src/data/cpd/fileops.ts
@@ -1,46 +1,59 @@
 // import * as xml from 'xml2js';
 // import * as fs from 'fs';
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 /**
  * Maintains information necessary to provide
  * appropriate link to duplicate code in other files.
  */
 export class OtherFile {
-    public readonly file: vscode.Uri;
-    public readonly line: number;
-    public readonly endLine: number;
-    public readonly column: number;
-    public readonly endColumn: number;
+  public readonly file: vscode.Uri;
+  public readonly line: number;
+  public readonly endLine: number;
+  public readonly column: number;
+  public readonly endColumn: number;
 
-    public constructor(file: vscode.Uri, line: number, endLine: number, column: number, endColumn: number) {
-        this.file = file;
-        this.line = line;
-        this.endLine = endLine;
-        this.column = column;
-        this.endColumn = endColumn;
-    }
+  public constructor(
+    file: vscode.Uri,
+    line: number,
+    endLine: number,
+    column: number,
+    endColumn: number
+  ) {
+    this.file = file;
+    this.line = line;
+    this.endLine = endLine;
+    this.column = column;
+    this.endColumn = endColumn;
+  }
 
-    get range(){
-        return new vscode.Range(this.line-1, this.column-1, this.endLine-1, this.endColumn-1);
-    }
+  get range() {
+    return new vscode.Range(
+      this.line - 1,
+      this.column - 1,
+      this.endLine - 1,
+      this.endColumn - 1
+    );
+  }
 
-    /**
-     * Same file link format that would be seen in the terminal.
-     * @returns text for the [] portions of the Markdown link.
-     */
-    public linkText(): string {
-        return vscode.workspace.asRelativePath(this.file) + ":" + this.line.toFixed(0);
-    }
+  /**
+   * Same file link format that would be seen in the terminal.
+   * @returns text for the [] portions of the Markdown link.
+   */
+  public linkText(): string {
+    return (
+      vscode.workspace.asRelativePath(this.file) + ":" + this.line.toFixed(0)
+    );
+  }
 
-    /**
-     * File link in a format that vscode will open and move to the 
-     * correct line.
-     * @returns proper link with anchor.
-     */
-    public link(): string {
-        return this.file + "#" + this.line.toFixed(0);
-    }
+  /**
+   * File link in a format that vscode will open and move to the
+   * correct line.
+   * @returns proper link with anchor.
+   */
+  public link(): string {
+    return this.file + "#" + this.line.toFixed(0);
+  }
 }
 
 // export class DuplicationFile {
@@ -61,51 +74,63 @@ export class OtherFile {
  * Contains the duplication data and function to retrieve the ranges
  */
 export class DuplicationData {
-    public readonly thisFile: vscode.Uri;
-    public readonly otherFiles: OtherFile[];
-    public readonly startLine: number;
-    public readonly endLine: number;
-    public readonly numTokens: number;
-    public readonly column: number;
-    public readonly endColumn: number;
+  public readonly thisFile: vscode.Uri;
+  public readonly otherFiles: OtherFile[];
+  public readonly startLine: number;
+  public readonly endLine: number;
+  public readonly numTokens: number;
+  public readonly column: number;
+  public readonly endColumn: number;
 
-    public constructor(thisFile: vscode.Uri, otherFiles: OtherFile[], startLine: number,
-        endLine: number, numTokens: number, column: number, endColumn: number) {
-        this.thisFile = thisFile;
-        this.otherFiles = otherFiles;
-        this.startLine = startLine;
-        this.endLine = endLine;
-        this.numTokens = numTokens;
-        this.column = column;
-        this.endColumn = endColumn;
-    }
+  public constructor(
+    thisFile: vscode.Uri,
+    otherFiles: OtherFile[],
+    startLine: number,
+    endLine: number,
+    numTokens: number,
+    column: number,
+    endColumn: number
+  ) {
+    this.thisFile = thisFile;
+    this.otherFiles = otherFiles;
+    this.startLine = startLine;
+    this.endLine = endLine;
+    this.numTokens = numTokens;
+    this.column = column;
+    this.endColumn = endColumn;
+  }
 
-    get range(){
-        return new vscode.Range(this.startLine-1, this.column-1, this.endLine-1, this.endColumn-1);
-    }
+  get range() {
+    return new vscode.Range(
+      this.startLine - 1,
+      this.column - 1,
+      this.endLine - 1,
+      this.endColumn - 1
+    );
+  }
 
-    /**
-     * Sets up the hover text and file links.
-     * @returns DecorationOptions containing the ranges and links to the other files
-     */
-    public getDecorationInformation() {
-        var msg = new vscode.MarkdownString("## This is duplicated:\r\n");
-        this.otherFiles.forEach((file) => {
-            if(file.file === this.thisFile){
-                msg.appendMarkdown("- [This File]](" + file.link() + ")\r\n");
-            } else {
-                msg.appendMarkdown("- [" + file.linkText() + "](" + file.link() + ")\r\n");
-            }
-           
-        });
-        //msg.isTrusted = true;
-        return {
-            hoverMessage: msg,
-            range: this.range
-        };
-    }
+  /**
+   * Sets up the hover text and file links.
+   * @returns DecorationOptions containing the ranges and links to the other files
+   */
+  public getDecorationInformation() {
+    var msg = new vscode.MarkdownString("## This is duplicated:\r\n");
+    this.otherFiles.forEach((file) => {
+      if (file.file.toString() === this.thisFile.toString()) {
+        msg.appendMarkdown(`- [This file at line ${file.line.toFixed(0)}](${file.link()})\r\n`);
+      } else {
+        msg.appendMarkdown(
+          `- [${file.linkText()}](${file.link()})\r\n`
+        );
+      }
+    });
+    //msg.isTrusted = true;
+    return {
+      hoverMessage: msg,
+      range: this.range,
+    };
+  }
 }
-
 
 /**
  * Make sure we have a full path to the file
@@ -113,16 +138,20 @@ export class DuplicationData {
  * @returns Uri to the file with workspace dir added if needed.
  */
 export function expandedUri(file: string): vscode.Uri {
-    if (file.match("[a-zA-Z]*://.*")) {
-        return vscode.Uri.parse(file);
-    }
-    var uri = vscode.Uri.file(file);
-    /** If the file is a relative assume it's from the workspace root
-         * and tweak as required
-         */
-    if (!file.startsWith("/") && !file.startsWith("file://") && vscode.workspace.workspaceFolders !== undefined) {
-        const workspaceDir = vscode.workspace.workspaceFolders[0].uri;
-        uri = vscode.Uri.file(workspaceDir.path + "/" + file);
-    }
-    return uri;
+  if (file.match("[a-zA-Z]*://.*")) {
+    return vscode.Uri.parse(file);
+  }
+  var uri = vscode.Uri.file(file);
+  /** If the file is a relative assume it's from the workspace root
+   * and tweak as required
+   */
+  if (
+    !file.startsWith("/") &&
+    !file.startsWith("file://") &&
+    vscode.workspace.workspaceFolders !== undefined
+  ) {
+    const workspaceDir = vscode.workspace.workspaceFolders[0].uri;
+    uri = vscode.Uri.file(workspaceDir.path + "/" + file);
+  }
+  return uri;
 }
--- src/gutter.ts
+++ src/gutter.ts
@@ -1,77 +1,82 @@
-import * as vscode from 'vscode';
-import { CodeAnalysisConfig } from './config';
-import { CPDCache } from './data/cpd/cache';
+import * as vscode from "vscode";
+import { CodeAnalysisConfig } from "./config";
+import { CPDCache } from "./data/cpd/cache";
 
 enum State {
-    renderOn,
-    renderOff
+  renderOn,
+  renderOff,
 }
 
 /**
  * Handles rendering PMD-CMD duplicate data
  */
 export class CPDGutters {
-    private duplicates: CPDCache;
-    private duplicateState: State;
-    private config: CodeAnalysisConfig;
+  private duplicates: CPDCache;
+  private duplicateState: State;
+  private config: CodeAnalysisConfig;
 
-    public constructor(duplicates: CPDCache, config: CodeAnalysisConfig,context:vscode.ExtensionContext) {
-        this.duplicates=duplicates;
-        this.config = config;
-        this.duplicateState = State.renderOff;
-        var onDupsChange = () => this.renderDuplicateGutters();
+  public constructor(
+    duplicates: CPDCache,
+    config: CodeAnalysisConfig,
+    context: vscode.ExtensionContext
+  ) {
+    this.duplicates = duplicates;
+    this.config = config;
+    this.duplicateState = State.renderOff;
+    var onDupsChange = () => this.renderDuplicateGutters();
 
-        this.duplicates.onChange(onDupsChange.bind(this));
+    this.duplicates.onChange(onDupsChange.bind(this));
+  }
+
+  public showDuplicates() {
+    this.duplicateState = State.renderOn;
+    var self = this;
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor !== undefined) {
+        self.renderDuplicateGutters();
+      }
+    });
+    this.renderDuplicateGutters();
+  }
+
+  public hideDuplicates() {
+    this.duplicateState = State.renderOff;
+    vscode.window.onDidChangeActiveTextEditor((e) => {});
+    this.renderDuplicateGutters();
+  }
+
+  private renderDuplicateGutters() {
+    const editor = vscode.window.activeTextEditor;
+    if (this.duplicateState === State.renderOff) {
+      editor?.setDecorations(this.config.cpdConfig.decTypeCritical, []);
+      editor?.setDecorations(this.config.cpdConfig.decTypeMajor, []);
+      editor?.setDecorations(this.config.cpdConfig.decTypeMinor, []);
+      return;
     }
 
-    public showDuplicates() {
-        this.duplicateState = State.renderOn;
-        var self = this;
-        vscode.window.onDidChangeActiveTextEditor((editor) => {
-			if (editor !== undefined) {
-				self.renderDuplicateGutters();
-			}
-		});
-        this.renderDuplicateGutters();
+    if (vscode.workspace.workspaceFolders !== undefined) {
+      if (editor !== null && editor !== undefined) {
+        const openFile = editor.document.fileName;
+        const duplicates = this.duplicates.getData(vscode.Uri.file(openFile));
+
+        var minor = new Array<vscode.DecorationOptions>();
+        var major = new Array<vscode.DecorationOptions>();
+        var critical = new Array<vscode.DecorationOptions>();
+
+        duplicates?.forEach((duplicate) => {
+          if (duplicate.numTokens < this.config.cpdConfig.minor) {
+            minor.push(duplicate.getDecorationInformation());
+          } else if (duplicate.numTokens < this.config.cpdConfig.major) {
+            major.push(duplicate.getDecorationInformation());
+          } else {
+            critical.push(duplicate.getDecorationInformation());
+          }
+        });
+
+        editor?.setDecorations(this.config.cpdConfig.decTypeMinor, minor);
+        editor?.setDecorations(this.config.cpdConfig.decTypeMajor, major);
+        editor?.setDecorations(this.config.cpdConfig.decTypeCritical, critical);
+      }
     }
-
-    public hideDuplicates() {
-        this.duplicateState = State.renderOff;
-        vscode.window.onDidChangeActiveTextEditor((e)=>{});
-        this.renderDuplicateGutters();
-    }
-
-    private renderDuplicateGutters() {
-        const editor = vscode.window.activeTextEditor;
-        if (this.duplicateState === State.renderOff) {
-            editor?.setDecorations(this.config.cpdConfig.decTypeCritical,[]);
-            editor?.setDecorations(this.config.cpdConfig.decTypeMajor,[]);
-            editor?.setDecorations(this.config.cpdConfig.decTypeMinor,[]);
-            return;
-        }
-
-        if (vscode.workspace.workspaceFolders !== undefined) {
-            if ( editor !== null && editor !== undefined) {
-                const openFile = editor.document.fileName;
-                const dups = this.duplicates.getData(vscode.Uri.file(openFile));
-                var minor = new Array<vscode.DecorationOptions>();
-                var major = new Array<vscode.DecorationOptions>();
-                var critical = new Array<vscode.DecorationOptions>();
-
-                dups?.forEach((dup) => {
-                    if (dup.numTokens < this.config.cpdConfig.minor) {
-                        minor.push(dup.getDecorationInformation());
-                    } else if (dup.numTokens < this.config.cpdConfig.major) {
-                        major.push(dup.getDecorationInformation());
-                    } else {
-                        critical.push(dup.getDecorationInformation());
-                    }
-                });
-
-                editor?.setDecorations(this.config.cpdConfig.decTypeMinor,minor);
-                editor?.setDecorations(this.config.cpdConfig.decTypeMajor,major);
-                editor?.setDecorations(this.config.cpdConfig.decTypeCritical,critical);
-            }
-        }
-    }
+  }
 }
